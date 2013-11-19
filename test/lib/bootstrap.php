628aa7c8f5e9771f22acc7551963538a49f1a090
emanuel <emanuel@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2013-11-19 23:53:04 +0000

    [resolve87] Use the external dependency on Mockit (rather than magic Mockit-location)
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@140 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- test/lib/bootstrap.php
+++ test/lib/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 
-//require_once(dirname(__FILE__)."/../../external/mockit/mockit/MockitTestCase.class.php");
+require_once(dirname(__FILE__)."/../../external/mockit/mockit/MockitTestCase.class.php");
 require_once(dirname(__FILE__)."/../../lib/PensioMerchantAPI.class.php");
+require_once(dirname(__FILE__)."/../../lib/PensioCallbackHandler.class.php");
