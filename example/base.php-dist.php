d7b730b1959d99416f3a764c21924b48520f5f6c
patrick <patrick@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2014-03-21 12:32:52 +0000

    Added some notes to the PHP example. The notes are based on some issues encountered today by a new merchant testing stuff using our example.
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@161 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- example/base.php-dist.php
+++ example/base.php-dist.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__).'/../PensioMerchantAPI.class.php');
 $baseURL = "https://testgateway.pensio.com/";
 $username = 'username';
 $password = 'password';
-$terminal = 'Some Terminal';
+$terminal = 'Some Terminal'; // change this to one of the test terminals supplied in the welcome email
 
 $api = new PensioMerchantAPI($baseURL, $username, $password, /*IPensioCommunicationLogger $logger = */null);
 
@@ -14,3 +14,15 @@ if(!$response->wasSuccessful())
 {
 	throw new Exception("Could not login to the Merchant API: ".$response->getErrorMessage());
 }
+
+
+/**
+ * If you get the following error when trying to login...
+ * SSL certificate problem: unable to get local issuer certificate
+ * ...then take a look at http://stackoverflow.com/a/19149687
+ *
+ * Basically you need to update your list of certificate authorities
+ * 1) Download http://curl.haxx.se/ca/cacert.pem
+ * 2) Add the following to your php.ini
+ *    curl.cainfo=<path-to>cacert.pem
+ */
