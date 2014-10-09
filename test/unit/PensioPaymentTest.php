29af950358ca37e3534dc69b0dd0e828bdb9d00d
rolandas <rolandas@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2014-10-09 07:55:35 +0000

    [44] Add tests for PensioAPIPayment
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@201 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- test/unit/PensioPaymentTest.php
+++ test/unit/PensioPaymentTest.php
@@ -3,21 +3,11 @@ require_once(dirname(__FILE__).'/../lib/bootstrap.php');
 
 class PensioPaymentTest extends MockitTestCase
 {
+	private $xml;
 	
 	public function setup()
 	{
-		
-	}
-	
-	public function testParsingOfSimpleXml()
-	{
-		$xml = new SimpleXMLElement('<Transaction><PaymentNatureService /><ReconciliationIdentifiers /></Transaction>');
-		$payment = new PensioAPIPayment($xml);
-	}
-
-	public function testParsing_of_CountryOfOrigin()
-	{
-		$xml = new SimpleXMLElement('
+		$this->xml = new SimpleXMLElement('
             <Transaction>
                 <TransactionId>14398495</TransactionId>
                 <AuthType>paymentAndCapture</AuthType>
@@ -37,7 +27,7 @@ class PensioPaymentTest extends MockitTestCase
                 <TransactionStatus>captured</TransactionStatus>
                 <MerchantCurrency>978</MerchantCurrency>
                 <CardHolderCurrency>978</CardHolderCurrency>
-                <ReservedAmount>19.95</ReservedAmount>
+                <ReservedAmount>20.00</ReservedAmount>
                 <CapturedAmount>19.95</CapturedAmount>
                 <RefundedAmount>0.00</RefundedAmount>
                 <RecurringDefaultAmount>0.00</RecurringDefaultAmount>
@@ -46,11 +36,14 @@ class PensioPaymentTest extends MockitTestCase
                 <PaymentNature>CreditCard</PaymentNature>
                 <PaymentSchemeName>Visa</PaymentSchemeName>
                 <PaymentNatureService name="ValitorAcquirer">
-                    <SupportsRefunds>true</SupportsRefunds>
-                    <SupportsRelease>true</SupportsRelease>
-                    <SupportsMultipleCaptures>true</SupportsMultipleCaptures>
-                    <SupportsMultipleRefunds>true</SupportsMultipleRefunds>
+                    <SupportsRefunds>true1</SupportsRefunds>
+                    <SupportsRelease>true2</SupportsRelease>
+                    <SupportsMultipleCaptures>true3</SupportsMultipleCaptures>
+                    <SupportsMultipleRefunds>true4</SupportsMultipleRefunds>
                 </PaymentNatureService>
+                <FraudRiskScore>42</FraudRiskScore>
+                <FraudExplanation>For the test fraud service the risk score is always equal mod 101 of the created amount for the payment</FraudExplanation>
+                <FraudRecommendation>Deny</FraudRecommendation>
                 <ChargebackEvents/>
                 <PaymentInfos>
                     <PaymentInfo name="item_name"><![CDATA[5 500 Gold]]></PaymentInfo>
@@ -64,8 +57,8 @@ class PensioPaymentTest extends MockitTestCase
                     <IpAddress>91.152.252.214</IpAddress>
                     <Email><![CDATA[timo.k.honkanen@elisanet.fi]]></Email>
                     <Username/>
-                    <CustomerPhone></CustomerPhone>
-                    <OrganisationNumber></OrganisationNumber>
+                    <CustomerPhone>22 22 22 22</CustomerPhone>
+                    <OrganisationNumber>345678</OrganisationNumber>
                     <CountryOfOrigin>
                         <Country>FI</Country>
                         <Source>CardNumber</Source>
@@ -80,8 +73,155 @@ class PensioPaymentTest extends MockitTestCase
                     </ReconciliationIdentifier>
                 </ReconciliationIdentifiers>
             </Transaction>');
-		$payment = new PensioAPIPayment($xml);
 
-		$this->assertEquals('FI', $payment->getCustomerInfo()->getCountryOfOrigin()->getCountry());
 	}
+	
+	public function testParsingOfSimpleXml()
+	{
+		$xml = new SimpleXMLElement('<Transaction><PaymentNatureService /><ReconciliationIdentifiers /></Transaction>');
+		$payment = new PensioAPIPayment($xml);
+	}
+
+	public function testParsing_of_CurrentStatus()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('captured', $payment->getCurrentStatus());
+	}
+
+	public function testParsing_of_Id()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('14398495', $payment->getId());
+	}
+
+	public function testParsing_of_AuthType()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('paymentAndCapture', $payment->getAuthType());
+	}
+
+	public function testParsing_of_ShopOrderId()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('ceae3968b82640e38a24ac162d8c2738', $payment->getShopOrderId());
+	}
+
+	public function testParsing_of_MaskedPan()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('424374******7275', $payment->getMaskedPan());
+	}
+
+	public function testParsing_of_CreditCardToken()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('37ad3ff596164142876df477e13336e0aeef0905', $payment->getCreditCardToken());
+	}
+
+	public function testParsing_of_CardStatus()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('Valid', $payment->getCardStatus());
+	}
+
+	public function testParsing_of_PaymentNature()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('CreditCard', $payment->getPaymentNature());
+	}
+
+	public function testParsing_of_PaymentSchemeName()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('Visa', $payment->getPaymentSchemeName());
+	}
+
+	public function testParsing_of_PaymentNatureService()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$paymentNature = $payment->getPaymentNatureService();
+		$this->assertEquals('ValitorAcquirer', $paymentNature->getName());
+		$this->assertEquals('true1', $paymentNature->getSupportsRefunds());
+		$this->assertEquals('true2', $paymentNature->getSupportsRelease());
+		$this->assertEquals('true3', $paymentNature->getSupportsMultipleCaptures());
+		$this->assertEquals('true4', $paymentNature->getSupportsMultipleRefunds());
+	}
+
+	public function testParsing_of_FraudRiskScore()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('42', $payment->getFraudRiskScore());
+	}
+
+	public function testParsing_of_FraudExplanation()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('For the test fraud service the risk score is always equal mod 101 of the created amount for the payment', $payment->getFraudExplanation());
+	}
+
+	public function testParsing_of_FraudRecommendation()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('Deny', $payment->getFraudRecommendation());
+	}
+
+	public function testParsing_of_CustomerInfo()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+		$customerInfo = $payment->getCustomerInfo();
+
+		$this->assertEquals('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0', $customerInfo->getUserAgent());
+		$this->assertEquals('91.152.252.214', $customerInfo->getIpAddress());
+		$this->assertEquals('timo.k.honkanen@elisanet.fi', $customerInfo->getEmail());
+		$this->assertEquals('22 22 22 22', $customerInfo->getPhone());
+		$this->assertEquals('345678', $customerInfo->getOrganisationNumber());
+		$this->assertEquals('FI', $customerInfo->getCountryOfOrigin()->getCountry());
+		$this->assertEquals('CardNumber', $customerInfo->getCountryOfOrigin()->getSource());
+	}
+
+	public function testParsing_of_PaymentInfo()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('5 500 Gold', $payment->getPaymentInfo('item_name'));
+		$this->assertEquals('19.95', $payment->getPaymentInfo('original_amount'));
+		$this->assertEquals('creditcard', $payment->getPaymentInfo('payment_method'));
+		$this->assertEquals('affe8e4f628ca55cbd07aa6b0b4fdffb', $payment->getPaymentInfo('signature'));
+		$this->assertEquals('eu', $payment->getPaymentInfo('wg_server'));
+	}
+
+	public function testParsing_of_Currency()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('978', $payment->getCurrency());
+	}
+
+	public function testParsing_of_ReservedAmount()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('20.00', $payment->getReservedAmount());
+	}
+
+	public function testParsing_of_CapturedAmount()
+	{
+		$payment = new PensioAPIPayment($this->xml);
+
+		$this->assertEquals('19.95', $payment->getCapturedAmount());
+	}
+
 }
