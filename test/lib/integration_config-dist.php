34b7756de1e6804f32dabb4cbd9d3265910b8730
sergej <sergej@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2016-01-21 10:05:13 +0000

    [g47] Changed name to AltaPay from Pensio
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@262 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- test/lib/integration_config-dist.php
+++ test/lib/integration_config-dist.php
@@ -3,5 +3,5 @@
 define('PENSIO_INTEGRATION_INSTALLATION', 'https://testgateway.pensio.com');
 define('PENSIO_INTEGRATION_USERNAME',     'username');
 define('PENSIO_INTEGRATION_PASSWORD',     'secret');
-define('PENSIO_INTEGRATION_TERMINAL',     'Pensio Test Terminal');
+define('PENSIO_INTEGRATION_TERMINAL',     'AltaPay Test Terminal');
 define('PENSIO_INTEGRATION_CURRENCY',     'EUR');
