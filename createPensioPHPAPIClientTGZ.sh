41b51fbb80860de7df7b40acda072b85649721e1
sergej <ss@pensio.dk>
2017-10-24 09:49:06 +0200

    [VESPLG-150] Removed sh file to generate tgz file as this is also done in build.xml

--- createPensioPHPAPIClientTGZ.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tar czf /tmp/PensioClientPHPAPI.tgz $(find . -type f | grep -v .svn | grep -v .idea | grep -v .settings | grep -v "./external/" | grep -v "./test/" | grep -v .buildpath | grep -v .project | grep -v build.xml | grep -v createPensioPHPAPIClientTGZ.sh |  sed 's/\.\///')
